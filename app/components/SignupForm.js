183e07cb400bf038cf42b6d7f07ca706bd4da847
Mike McCready <michaeljmccready@gmail.com>
2016-05-02 17:49:16 -0700

    add password check handler

--- app/components/SignupForm.js
+++ app/components/SignupForm.js
@@ -1,9 +1,39 @@
 var React = require('react');
 
 function SignupForm(props) {
-		return(
-			<h1>Signup</h1>
-		)
+	console.log(props)
+	return(
+		<form className="form-container">
+			<label for="name">
+				Name
+				<input type="text" 
+					placeholder="John Smith" 
+					autocomplete="name" 
+					autoFocus={focus}
+					required>
+				</input>
+			</label>
+			<label for="email">
+				Email address
+				<input id="email" 
+					type="email" 
+					placeholder="john.smith@gmail.com" 
+					autocomplete="email" 
+					required></input>
+			</label>
+			<label for="password">
+				Password
+				<input id="password" 
+					type="password" 
+					placeholder="Password1234" 
+					autocomplete="new-password" 
+					required
+					onChange={props.onCheckPassword}
+					value={props.password}></input>
+			</label>			
+		</form>
+	)
 }
+
 
 module.exports = SignupForm;
--- app/containers/SignupContainer.js
+++ app/containers/SignupContainer.js
@@ -2,9 +2,31 @@ var React = require('react');
 var SignupForm = require('../components/SignupForm');
 
 var SignupContainer = React.createClass({
+	getInitialState: function() {
+		return{
+			userInfo: {
+				name: '',
+				email: '',
+				password: ''
+			}
+		}
+	},
+	handleCheckPassword: function(e) {
+		console.log(this.state.userInfo.password)
+		var newUserInfo = this.state.userInfo;
+		newUserInfo.password = e.target.value;
+	    this.setState({
+	      userInfo : newUserInfo
+	    });
+	},
 	render: function() {
 		return(
-			<SignupForm />
+			<div>
+				<h1>Sign up</h1>
+				<SignupForm 
+					password={this.state.userInfo.password}
+					onCheckPassword={this.handleCheckPassword}/>
+			</div>	
 		)
 	}
 });

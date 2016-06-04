4ce194c26b7059b58f16e4ce6734b8122d154c07
Mike McCready <michaeljmccready@gmail.com>
2016-06-04 11:37:17 -0700

    update welcome page

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -7,7 +7,7 @@ function Welcome() {
 		<div className="welcome-page">
 			<div className="text-block">
 				<h1>Meetups</h1>
-				<Link className='welcome-link' to='/signup'>Let's Party</Link>
+				<Link className='welcome-link' to='/events'>Let's Party</Link>
 			</div>	
 		</div>
 	)
